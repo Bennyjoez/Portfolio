86ca5c25e3df9d402199f9eead558a5c7b7509dd
Benson Njuguna <njugunab655@gmail.com>
2023-03-07 18:08:02 +0300

    Specify function to close btn

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const hamburgerIcon = document.querySelector("#hamburger-menu");
 const hamburgerContentContainer = document.querySelector(".links");
+const closeHamburgerBtn = document.querySelector("#close-hamburger > i");
 
 hamburgerIcon.addEventListener("click", showMenu);
+closeHamburgerBtn.addEventListener("click", closeMenu);
 
 function showMenu() {
   hamburgerContentContainer.classList.add("hamburger-content");
   
   // get nav-links in hamburger view
-  const navLinks = document.querySelectorAll(".hamburger-content li");
+  const navLinks = document.querySelectorAll(".hamburger-content li:not(#close-hamburger)");
   navLinks.forEach(navigationClose);
 }
 
