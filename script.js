bb009f44e726456043fcb18f1588ddbb9dfc8982
Benson Njuguna <njugunab655@gmail.com>
2023-03-07 13:38:01 +0300

    Add close function to close hamburger

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let hamburgerIcon = document.querySelector("#hamburger-menu");
 let hamburgerContentContainer = document.querySelector(".links");
+const closeHamburgerBtn = document.querySelector("#close-hamburger");
 
 hamburgerIcon.addEventListener("click", showMenu);
+closeHamburgerBtn.addEventListener("click", closeMenu)
 
 function showMenu() {
   hamburgerContentContainer.classList.toggle("hamburger-content");
   hamburgerContentContainer.style.display = "block";
+}
+
+function closeMenu() {
+  hamburgerContentContainer.classList.toggle("hamburger-content");
+  hamburgerContentContainer.style.display = "none";
 }
