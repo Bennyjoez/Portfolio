a35effd5d8509d162c52075f3738671888fd5ab0
Benson Njuguna <njugunab655@gmail.com>
2023-03-08 14:21:37 +0300

    Add project cards' details object

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+const projects = [
+  {
+    name: "Multi-Post Stories",
+    featured: true,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/multi-post-stories.png",
+      alt: "Background image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  },
+  {
+    name: "Professional Art Printing Data",
+    featured: false,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    featuredImage: {
+      link: "images/background2.png",
+      alt: "Background Image"
+    },
+    technologies: ["html", "Bootstrap", "Ruby on rails"],
+    liveLink: "#",
+    sourceLink: "#"
+  }
+]
+
 const hamburgerIcon = document.querySelector('#hamburger-menu');
 const hamburgerContentContainer = document.querySelector('.links');
 const closeHamburgerBtn = document.querySelector('#close-hamburger > i');
