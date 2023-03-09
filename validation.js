da6161e8d396553638ab6ccb22e17dccd32c1e40
OsmanFP <osmanwako411@gmail.com>
2023-03-09 18:16:31 +0300

    create function to check valid input

--- validation.js
+++ validation.js
@@ -1,7 +1,15 @@
 const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
+const message = document.getElementById('errormessage');
 
-function isvalidEmail() {
-  return true;
+function isvalidEmail(event) {
+  event.preventDefault();
+  return false;
+}
+
+function showErrorMessage() {
+  email.classList.toggle('invalidinput');
+  message.classList.toggle('errormessage');
+  message.textContent = 'You should have to use ';
 }
 form.addEventListener('submit', isvalidEmail);
