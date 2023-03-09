57986b0dc8094533f57f9e2288686dedd5747a5f
Benson Njuguna <njugunab655@gmail.com>
2023-03-09 19:24:41 +0300

    Validate email is in small letters

--- validation.js
+++ validation.js
@@ -2,14 +2,27 @@ const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
 const message = document.getElementById('errormessage');
 
-function isvalidEmail(event) {
-  event.preventDefault();
-  return false;
+function showErrorMessage() {
+  const ERROR_MESSAGE = 'You have to use small letters for an email!';
+  email.className = 'invalidinput';
+  message.textContent = ERROR_MESSAGE;
 }
 
-function showErrorMessage() {
-  email.classList.toggle('invalidinput');
-  message.classList.toggle('errormessage');
-  message.textContent = 'You should have to use ';
+function showSuccess() {
+  email.classList.remove('invalidinput');
+  message.textContent = '';
+  form.submit();
 }
+
+function isvalidEmail(event) {
+  event.preventDefault();
+  const emailRegex = /[A-Z]/g;
+  const tested = email.value;
+  if (emailRegex.test(tested)) {
+    showErrorMessage();
+  } else {
+    showSuccess();
+  }
+}
+
 form.addEventListener('submit', isvalidEmail);
