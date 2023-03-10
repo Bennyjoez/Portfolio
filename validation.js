9f5f1acb84f570b5ddc42126d0c7b6f0b2de8697
OsmanFP <osmanwako411@gmail.com>
2023-03-10 13:32:51 +0300

    Update keyup event listener to change listener

--- validation.js
+++ validation.js
@@ -1,6 +1,13 @@
 const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
 const message = document.getElementById('errormessage');
+const named = document.querySelector('#name');
+const description = document.querySelector('#message');
+const messageObj = {
+  username: `${named.value}`,
+  email: `${email.value}`,
+  message: `${description.value}`,
+};
 
 function showErrorMessage() {
   const ERROR_MESSAGE = 'You have to use small letters for an email!';
@@ -27,25 +34,19 @@ function isvalidEmail(event) {
 
 form.addEventListener('submit', isvalidEmail);
 
-const named = document.querySelector("#name");
-const messageObj = {
-  username: `${named.value}`,
-  email: `${email.value}`,
-}
-
 function updateMessage(input, value) {
   messageObj[input] = value;
   const objString = JSON.stringify(messageObj);
-  localStorage["messageObj"] = objString;
+  localStorage['messageObj'] = objString;
 }
 
 function populateStorage(messageObj) {
   const objString = JSON.stringify(messageObj);
-  localStorage.setItem("messageObj", objString);
+  localStorage.setItem('messageObj', objString);
 }
 
 function validateStorage(messageObj) {
-  if(!localStorage.getItem(messageObj)) {
+  if (!localStorage.getItem(messageObj)) {
     return false;
   } else {
     return true;
@@ -53,18 +54,16 @@ function validateStorage(messageObj) {
 }
 
 function respond(e) {
-  setTimeout(() => {
-    let value = e.target.value;
-    let input = e.target.name;
-    if (validateStorage('messageObj')) {
-      updateMessage(input, value);
-    } else {
-      messageObj.email = email.value;
-      messageObj.name = named.value;
-      populateStorage(messageObj);
-    }
-  }, 1000)
+  let value = e.target.value;
+  let input = e.target.name;
+  if (validateStorage('messageObj')) {
+    updateMessage(input, value);
+  } else {
+    messageObj[input] = value;
+    populateStorage(messageObj);
+  }
 }
 
-named.addEventListener('keyup', respond);
-email.addEventListener('keyup', respond);
+named.addEventListener('change', respond);
+email.addEventListener('change', respond);
+description.addEventListener('change', respond);
